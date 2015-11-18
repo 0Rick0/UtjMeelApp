414096d15584f3af06e117bfd935419cb5c84f16
Rick <rick-boy1@live.nl>
2015-11-18 16:43:29 +0100

    Implemented HTTP get/post methods, not tested

--- app/src/main/java/tk/r_ware/utjmeelapp/Communication/Communication.java
+++ app/src/main/java/tk/r_ware/utjmeelapp/Communication/Communication.java
@@ -5,19 +5,26 @@ import android.content.Context;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -151,12 +158,12 @@ public class Communication {
         JSONObject result = doPostRequest(addr,data);
         try {
             int error = result.getInt("error");
-            if(error>0){
+            if(error!=0){
                 lastError = result.getString("error_text");
             }
             this.username = result.getString("username");
             this.token = result.getString("session_id");
-            DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
             this.expirationDate = format.parse(result.getString("expiration_date-date"));
         } catch (JSONException e) {
             e.printStackTrace();
@@ -172,10 +179,114 @@ public class Communication {
     }
 
     private JSONObject doGetRequest(String addr){
-        return new JSONObject();
+        HttpURLConnection c = null;
+        try {
+            URL u = new URL(addr);
+            c = (HttpURLConnection) u.openConnection();
+            c.setRequestMethod("GET");
+            c.setRequestProperty("Content-length", "0");
+            c.setUseCaches(false);
+            c.setAllowUserInteraction(false);
+            c.setConnectTimeout(10000);
+            c.setReadTimeout(10000);
+            c.connect();
+            int status = c.getResponseCode();
+
+            switch (status) {
+                case 200:
+                case 201:
+                    BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line+"\n");
+                    }
+                    br.close();
+                    return new JSONObject(sb.toString());
+            }
+
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (c != null) {
+                try {
+                    c.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        JSONObject ret = null;
+        try {
+            ret = new JSONObject().put("error", -5).put("error_text","Failed to connect");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return ret;
     }
 
     private JSONObject doPostRequest(String addr, Map<String,String> vars){
-        return new JSONObject();
+        HttpURLConnection c = null;
+        try {
+            URL u = new URL(addr);
+            c = (HttpURLConnection) u.openConnection();
+            c.setRequestMethod("POST");
+            c.setRequestProperty("Content-length", "0");
+            c.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+            c.setUseCaches(false);
+            c.setAllowUserInteraction(false);
+            c.setDoInput(true);
+            c.setDoOutput(true);
+            c.setConnectTimeout(10000);
+            c.setReadTimeout(10000);
+
+            OutputStream os = c.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+            StringBuilder vb = new StringBuilder();
+            int i = 0;
+            //build an query string
+            for(Map.Entry<String,String> kvp : vars.entrySet()){
+                vb.append(kvp.getKey()).append("=").append(kvp.getValue());
+                i++;
+                if(i!=vars.size()){
+                    vb.append("&");
+                }
+            }
+            writer.write(vb.toString());
+
+            c.connect();
+            int status = c.getResponseCode();
+
+            switch (status) {
+                case 200:
+                case 201:
+                    BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line+"\n");
+                    }
+                    br.close();
+                    return new JSONObject(sb.toString());
+            }
+
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (c != null) {
+                try {
+                    c.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        JSONObject ret = null;
+        try {
+            ret = new JSONObject().put("error", -5).put("error_text","Failed to connect");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return ret;
     }
 }

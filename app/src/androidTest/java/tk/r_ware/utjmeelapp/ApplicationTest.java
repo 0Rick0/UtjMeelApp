e0658e9fac2181813ac80d7972739cae2a710060
Rick <rick-boy1@live.nl>
2015-12-10 21:29:42 +0100

    Added some basic tests

--- app/src/androidTest/java/tk/r_ware/utjmeelapp/ApplicationTest.java
+++ app/src/androidTest/java/tk/r_ware/utjmeelapp/ApplicationTest.java
@@ -3,11 +3,33 @@ package tk.r_ware.utjmeelapp;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import tk.r_ware.utjmeelapp.Communication.Communication;
+import tk.r_ware.utjmeelapp.Communication.containers.Info;
+import tk.r_ware.utjmeelapp.Communication.containers.Statistics;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Test
+    public void test(){
+        Communication instance = Communication.getInstance();
+        //test login method
+        Assert.assertFalse(instance.login("UtjMeel", "notAdmin"));
+        Assert.assertTrue(instance.login("UtjMeel", "admin"));
+
+        Info info = instance.info();//get info
+        Info cInfo = instance.getCachedInfo();
+        Assert.assertSame(info,cInfo);//check if chased info is the same as normal info
+
+        Statistics stats = instance.statistics();
     }
 }
